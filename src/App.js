544af4c43cb4d05649bb51d6d640ee5559c0fbea
Huynh Anh Kiet <huynhanhkiet179@gmail.com>
2022-03-23 23:54:43 +0700

    firebase dep

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <h1 className='text-3xl font-bold text-blue-700'>My to-do</h1>
   );
 }
 
