ae5b38092fde2a33eab92593632788f708a09ce4
Huynh Anh Kiet <huynhanhkiet179@gmail.com>
2022-04-04 23:49:55 +0700

    not-found page and transition

--- src/index.js
+++ src/index.js
@@ -5,20 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthenticatingEntry from './pages/authenticate-entry.page';
+import NotFoundPage from './pages/not-found.page';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/sign-in" element={<AuthenticatingEntry action="sign-in" />} />
-        <Route path="/sign-up" element={<AuthenticatingEntry action="sign-up" />} />
+        <Route path="sign-in" element={<AuthenticatingEntry action="sign-in" />} />
+        <Route path="sign-up" element={<AuthenticatingEntry action="sign-up" />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// get user data by navigating to path '/userId'
+// get user data about list todo by navigating path '/userId/listId'
+// get user data about specific task in a list by navigatin to path '/userId/listId/taskId'
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
